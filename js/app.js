74bbf6ab9441495634fccc32b79948a57cf61b0e
francojnieva <franco97nieva@hotmail.com>
2023-06-05 15:30:18 -0300

    :hammer: update logic of if conditionals

--- js/app.js
+++ js/app.js
@@ -7,15 +7,24 @@ btnStart.addEventListener("click", () => {
     const numRamdom = Math.floor(Math.random()*100)+1
 
     btnSend.addEventListener("click", () => {
+        
         if (numRamdom == numberAdd.value) {
-          alerts.innerHTML = `<div class="alert alert-success text-center" role="alert">
+           return alerts.innerHTML = `<div class="alert alert-success text-center" role="alert">
            ¡Felicidades, adivinaste el Número Mágico!
         </div>`
-        } else {
-            alerts.innerHTML = `<div class="alert alert-warning text-center" role="alert">
-            No adivinaste el Número Mágico.
-        </div> `
-        }
+        } 
+
+        if (numRamdom !== numberAdd.value && numRamdom > numberAdd.value) {
+            return alerts.innerHTML = `<div class="alert alert-warning text-center" role="alert">
+            No adivinaste. <br> Pista: El Número Mágico es mayor. 
+            </div>`
+        } 
+
+        if (numRamdom !== numberAdd.value && numRamdom < numberAdd.value) {
+            return alerts.innerHTML = `<div class="alert alert-warning text-center" role="alert">
+            No adivinaste. <br> Pista: El Número Mágico es menor. 
+            </div>`
+        } 
     })
 })
 
